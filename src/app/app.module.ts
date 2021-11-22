aec4f22bd10f256caef43ac79214d2e8fee5882d
Bilel Bouazizi <bilel.bouazizi@gmail.com>
2021-11-22 01:03:11 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,27 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import{HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ProductAddComponent} from "./components/product-add/product-add.component";
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    ProductsComponent
-   
+    ProductsComponent,
+    ProductAddComponent,
+    ProductEditComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/model/product.model';
-import { ProductsService } from 'src/app/services/products.service';
+import {ProductsService} from '../../services/products.service';
+import {Product} from '../../model/product.model';
+import {Observable, of} from 'rxjs';
+import {catchError, map, startWith} from 'rxjs/operators';
+import {AppDataState, DataStateEnum} from '../../state/product.state';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -8,18 +12,79 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  //notre liste de produit peut etre null et on initialize with null
-  products:Product[]|null=null;
+  products$:Observable<AppDataState<Product[]>> |null=null;
+  readonly DataStateEnum=DataStateEnum;
 
-  constructor(private productService:ProductsService) { }
+  constructor(private productsService:ProductsService, private router:Router) { }
 
   ngOnInit(): void {
   }
-  OnGetAllProducts(){
-    this.productService.getAllProducts().subscribe(data=>{
-      this.products=data;
-    },err =>{
-      console.log(err);
-    })
+
+  onGetAllProducts() {
+    this.products$= this.productsService.getAllProducts().pipe(
+      map(data=>{
+        console.log(data);
+        return ({dataState:DataStateEnum.LOADED,data:data})
+      }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(err=>of({dataState:DataStateEnum.ERROR, errorMessage:err.message}))
+    );
+  }
+
+  onGetSelectedProducts() {
+    this.products$= this.productsService.getSelectedProducts().pipe(
+      map(data=>{
+        console.log(data);
+        return ({dataState:DataStateEnum.LOADED,data:data})
+      }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(err=>of({dataState:DataStateEnum.ERROR, errorMessage:err.message}))
+    );
+  }
+
+  onGetAvailableProducts() {
+    this.products$= this.productsService.getAvailableProducts().pipe(
+      map(data=>{
+        console.log(data);
+        return ({dataState:DataStateEnum.LOADED,data:data})
+      }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(err=>of({dataState:DataStateEnum.ERROR, errorMessage:err.message}))
+    );
+  }
+
+  onSearch(dataForm: any) {
+    this.products$= this.productsService.searchProducts(dataForm.keyword).pipe(
+      map(data=>{
+        console.log(data);
+        return ({dataState:DataStateEnum.LOADED,data:data})
+      }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(err=>of({dataState:DataStateEnum.ERROR, errorMessage:err.message}))
+    );
+  }
+
+  onSelect(p: Product) {
+    this.productsService.select(p)
+      .subscribe(data=>{
+        p.selected=data.selected;
+      })
+  }
+
+  onDelete(p: Product) {
+    let v=confirm("Etes vous sûre?");
+    if(v==true)
+      this.productsService.deleteProduct(p)
+        .subscribe(data=>{
+          this.onGetAllProducts();
+        })
+  }
+
+  onNewProduct() {
+    this.router.navigateByUrl("/newProduct");
+  }
+
+  onEdit(p: Product) {
+    this.router.navigateByUrl("/editProduct/"+p.id);
   }
 }
